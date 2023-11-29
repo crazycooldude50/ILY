4a9680441a1d184f6329ab727bfdf96d2788551c
Cloudz <55644446+crazycooldude50@users.noreply.github.com>
2023-11-28 17:06:16 -0800

    Update BarGraph.java
    ADWAJDWAUDJA

--- BarGraph.java
+++ BarGraph.java
@@ -10,9 +10,9 @@ public class BarGraph extends JFrame {
 
    public BarGraph(Map<String, Integer> data) {
    
-      setSize(1500, 600);
+      // Create the window
+      setSize(1150, 600);
       setResizable(false);
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
    
       BarChartPanel chartPanel = new BarChartPanel(data);
       chartPanel.setPreferredSize(new Dimension(400, 300));
@@ -20,7 +20,9 @@ public class BarGraph extends JFrame {
    }
 
    public static void main(String[] args) {
-      Map<String, Integer> data = new LinkedHashMap<>();
+      Map<String, Integer> data = new LinkedHashMap<>(); // LinkedHashMap to store the x and y value of each bar
+      
+      // Add each day of the week to the LinkedHashMap
       data.put("Monday", 5);
       data.put("Tuesday", 8);
       data.put("Wednesday", 3);
@@ -44,29 +46,30 @@ class BarChartPanel extends JPanel {
    public void paintComponent(Graphics g) {
       super.paintComponent(g);
    
+      // Declare and initialize variables to store some basic values
       int barWidth = 100;
       int barGap = 40;
-      int startX = 100;
+      int startX = 70;
       int endX = getWidth() - 50;
       int startY = getHeight() - 50;
       int endY = 50;
    
-      int maxValue = data.values().stream().max(Integer::compare).orElse(0);
+      int maxValue = data.values().stream().max(Integer::compare).orElse(0); // Tallest bar
    
-      Axes(g, startX, endX, startY, endY, maxValue);
+      Axes(g, startX, endX, startY, endY, maxValue); // Create, title and scale the two axes
    
       g.setColor(Color.blue);
-      int x = startX + barGap;
+      int x = startX + barGap; // Add some distance between the first bar and the y-axis
    
-      for (Map.Entry<String, Integer> entry : data.entrySet()) {
-         String category = entry.getKey();
-         int value = entry.getValue();
+      for (Map.Entry<String, Integer> entry : data.entrySet()) { // For-each loop to traverse through the LinkedHashMap
+         String category = entry.getKey(); // Retrieve the x value
+         int value = entry.getValue(); // Retrieve the y value
          int barHeight = (int) ((double) value / maxValue * (startY - endY));
       
-         g.fillRect(x, startY - barHeight, barWidth, barHeight);
+         g.fillRect(x, startY - barHeight, barWidth, barHeight); // Create the bar
          g.setColor(Color.black);
       
-         int labelX = x + barWidth / 2 - g.getFontMetrics().stringWidth(category) / 2;
+         int labelX = x + barWidth / 2 - g.getFontMetrics().stringWidth(category) / 2; // Label the x axis for the new bar
          g.drawString(category, labelX, startY + 15);
          
          x += barWidth + barGap;
@@ -75,28 +78,34 @@ class BarChartPanel extends JPanel {
    }
   
    public void Axes(Graphics g, int startX, int endX, int startY, int endY, int maxValue) {
+      // Create the two axes
       g.drawLine(startX, startY, endX, startY);
       g.drawLine(startX, startY, startX, endY - 25);
       
-      Font basicFont = new Font(g.getFont().getFontName(), Font.PLAIN, 12);
-      Font axisLabelFont = new Font(g.getFont().getFontName(), Font.PLAIN, 18);
       
-      g.setColor(Color.RED);
+      Font basicFont = new Font(g.getFont().getFontName(), Font.PLAIN, 12); // Store the basic font to fall back to once done with axes titles
+      Font axisLabelFont = new Font(g.getFont().getFontName(), Font.PLAIN, 18); // Bigger font for the axes title
+      g.setColor(Color.RED); // Make the axes title font red
       
+      // Title the x-axis
       g.setFont(axisLabelFont);
-      g.drawString("Day", startX + ((endX - startX) / 2), startY + 40);
+      g.drawString("Day", startX + ((endX - startX) / 2), startY + 40); 
      
-      AffineTransform affineTransform = new AffineTransform();
-      affineTransform.rotate(-Math.PI / 2, 0, 0);
-      Font rotatedFont = basicFont.deriveFont(affineTransform);
+      // Use AffineTransform to rotate the text for the title for the y-axis
+      AffineTransform affineTransform = new AffineTransform(); 
+      affineTransform.rotate(-Math.PI / 2);
+      Font rotatedFont = axisLabelFont.deriveFont(affineTransform);
+      
+      // Title the y-axis
       g.setFont(rotatedFont);
-      g.drawString("Events", startX - 40, endY - 30);
+      g.drawString("Events", startX - 40, startY - ((startY - endY) / 2)); 
+      
+      // Return to default
       g.setFont(basicFont);
-      
       g.setColor(Color.BLACK);
-      
+     
       for (int i = 0; i <= maxValue; i++) {
-         g.drawString(Integer.toString(i), startX - 30, startY - (i * ((startY-endY)/maxValue)));
+         g.drawString(Integer.toString(i), startX - 25, startY - (i * ((startY-endY)/maxValue))); // Label the y-axis in a linear fashion starting from 0 till the height of the tallest bar
       }
    }
 }
